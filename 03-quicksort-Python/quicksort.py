29a5947151b63f161b8581741a2ff98278daa769
Rachana <rachanabalabadra29@msitprogram.net>
2021-08-17 11:03:31 +0530

    quicksort

--- 03-quicksort-Python/quicksort.py
+++ 03-quicksort-Python/quicksort.py
@@ -1,6 +1,29 @@
 """Implement quick sort in Python.
 Input a list.
 Output a sorted list."""
-def quicksort(array):
+def quicksort(array,start,end):
 	# Your code goes here
-	pass
+	if(end-start)>1:
+		p=partition(array,start,end)
+		quicksort(array,start,p)
+		quicksort(array,p+1,end)
+
+def partition(array,start,end):
+	pivot=array[start]
+	i=start+1
+	j=end-1
+	while True:
+		while(i<=j and array[i]<=pivot):
+			i+=1
+		while(i<=j and array[j]>=pivot):
+			j-=1
+		if(i<=j):
+			array[i],array[j]=array[j],array[i]
+		else:
+			array[start],array[j]=array[j],array[start]
+			return j
+array=input()
+start=0
+end=len(array)
+quicksort(array,start,end)
+print(array)
