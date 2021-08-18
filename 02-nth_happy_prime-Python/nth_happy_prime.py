aee59c23d597af39cf63eb2bc9f1028751c142a7
Rachana <rachanabalabadra29@msitprogram.net>
2021-08-18 22:39:06 +0530

    nthhappyprime

--- 02-nth_happy_prime-Python/nth_happy_prime.py
+++ 02-nth_happy_prime-Python/nth_happy_prime.py
@@ -3,6 +3,45 @@
 # and returns the nth happy prime number (where the 0th happy prime number is 7).
 
 
+def prime(n):
+    for i in range(2,n):
+        if(n%i==0):
+            return False
+    return True
 
+
+def square(n):
+    sqnum=0
+    while(n):
+        sqnum += (n%10)*(n%10)
+        n=n//10
+    return sqnum
+
+def happy(n):
+    s=n
+    f=n
+    while True:
+        s=square(s)
+        f=square(square(f))
+        if(s!=f):
+           continue
+        else:
+           break
+    if(s==1):
+        return True
+    else:
+        return False
+
+def happyprime(n):
+    if(not prime(n)):
+        return False
+    return happy(n)
+    
 def fun_nth_happy_prime(n):
-	return 0
+    c=-2
+    i=0
+    while(c<n):
+        if(happyprime(i)==True and prime(i)==True):
+            c+=1
+        i+=1
+    return i-1
