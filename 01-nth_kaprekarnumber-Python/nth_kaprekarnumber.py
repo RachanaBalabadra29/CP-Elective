360c2e1e80744e133987a63e639857a19b73e2e1
Rachana <rachanabalabadra29@msitprogram.net>
2021-08-20 12:24:11 +0530

    nth_kaprekarnumber

--- 01-nth_kaprekarnumber-Python/nth_kaprekarnumber.py
+++ 01-nth_kaprekarnumber-Python/nth_kaprekarnumber.py
@@ -9,5 +9,42 @@
 
 import math
 
+def kaprekar(n):
+    c=str(n)
+    a=n*n #2025 #81
+    i=0
+    digits=""
+    res1=0
+    res3=0
+    while(i<len(c)):
+        r=a%10 #1
+        i+=1
+        digits+=str(r) #1
+        a=a//10 #8
+    res=a+int(digits[::-1]) #81
+    k=a
+    j=k%10
+    
+    u=a
+    l=u%10
+    u=u//10
+    l1=u%10
+    if(l==0 and l1==0):
+        u=u//10
+        res3=u+int(digits[::-1])
+    if(j==0):
+        k=k//10
+        res1=k+int(digits[::-1])
+    if(n==res or n==res1 or n==res3):
+        return True
+    else:
+        return False
+
 def fun_nth_kaprekarnumber(n):
-    return 1;
+    c=0
+    i=1
+    while(c<=n):
+        if(kaprekar(i)==True):
+            c+=1
+        i+=1
+    return i-1
