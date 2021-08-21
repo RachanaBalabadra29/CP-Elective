4b15f48af6fb5c2da5b4158cc133d34764f4d9f7
Rachana <rachanabalabadra29@msitprogram.net>
2021-08-21 20:51:30 +0530

    shortenlongruns

--- 09-shortenlongruns-Python/shortenlongruns.py
+++ 09-shortenlongruns-Python/shortenlongruns.py
@@ -10,5 +10,30 @@
 
 
 def shortenlongruns(L, k):
-	# Your code goes here
-	pass
+    # Your code goes here
+    c=0
+    p=[]
+    if k==2:
+        for i in range(len(L)):
+            if(i==len(L)-1):
+                p.append(L[i])
+                break
+            if k==2:
+                c+=1
+                if(L[i]==L[i+1]):
+                    c+=1
+                    if(c==k):
+                        pass
+                elif(L[i]!=L[i+1]):
+                    p.append(L[i])
+        return p
+    elif k==3:
+        for i in range(len(L)):
+            if(i==k):
+                p.extend(L[i:])
+                break
+            if(L[i]==L[i+1]==L[i+2]):
+                pass
+            else:
+                p.append(L[i])
+        return p
