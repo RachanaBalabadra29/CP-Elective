af1f09db12504d84a64ea5a8404a96ae75e2c302
Rachana <rachanabalabadra29@msitprogram.net>
2021-08-25 19:42:47 +0530

    recursion_binarysearchvalues

--- 05-recursion_binarysearchvales-Python/recursion_binarysearchvalues.py
+++ 05-recursion_binarysearchvales-Python/recursion_binarysearchvalues.py
@@ -17,7 +17,56 @@
 #     assert(binarySearchValues(L, v) == [(2,'f'), (0,'a'), (1,'c')])
 # Hint: Do not slice the list L, but rather adjust the indexes into L. 
 
-def recursion_binarysearchvalues(L, v):
-	# Your codes goes here
-	pass
-	
+# def recursion_binarysearchvalues(L, v):
+# 	# Your codes goes here
+# 	low=0
+# 	high=len(L)-1
+# 	p=[]
+# 	while(low<=high):
+# 		mid=(low+high)//2
+# 		if(L[mid]<v):
+# 			p.append((mid,L[mid]))
+# 			low=mid+1
+# 		elif(L[mid]>v):
+# 			p.append((mid,L[mid]))
+# 			high=mid-1
+# 		else:
+# 			p.append((mid,L[mid]))
+# 			return p
+# 	return p
+
+
+
+def binary_search(L, low, high, v,p=[]):
+    if high >= low:
+        mid = (high + low) // 2
+        if L[mid] == v:
+            p.append((mid,L[mid]))
+            return p
+        elif L[mid] > v:
+            p.append((mid,L[mid]))
+            return binary_search(L, low, mid - 1, v, p)
+        else:
+            p.append((mid,L[mid]))
+            return binary_search(L, mid + 1, high, v, p)
+    else:
+        return p
+        
+        
+def recursion_binarysearchvalues(L,v):
+    # your code goes here
+	A = []
+	binary_search(L, 0, len(L)-1, v, A)
+	return A
+
+
+L = ['a', 'c', 'f', 'g', 'm', 'q']
+v = 'z'
+print(recursion_binarysearchvalues(L,'a'))
+print(recursion_binarysearchvalues(L,'c'))
+print(recursion_binarysearchvalues(L,'f'))
+print(recursion_binarysearchvalues(L,'g'))
+print(recursion_binarysearchvalues(L,'m'))
+print(recursion_binarysearchvalues(L,'q'))
+print(recursion_binarysearchvalues(L,'z'))
+print(recursion_binarysearchvalues(L,'b'))
