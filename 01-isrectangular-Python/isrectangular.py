993f57f5767fb1240be8014ee90369b0c33acf5e
Rachana <rachanabalabadra29@msitprogram.net>
2021-08-25 12:20:35 +0530

    isrectangular

--- 01-isrectangular-Python/isrectangular.py
+++ 01-isrectangular-Python/isrectangular.py
@@ -3,9 +3,29 @@
 #  the same number of elements. Return False otherwise.
 
 
+
+#([[1, 2, 3], [1, 1]], False), ([[1, 2, 3], [1, 1, 2]], True),
+#([[], []], True), ([[1, 2], [1, 1], [1, 2, 3]], False)
+
 def fun_isrectangular(l):
 	# Your code goes here...
-	return False
+	c=0
+	if(len(l)==0):
+		return True
+	p=[]
+	for i in range(len(l)):
+		c=len(l[i])
+		p.append(c)
+	 
+	 
+	s=[]   
+	for i in p:
+	    if i not in s:
+	        s.append(i)
+	if(len(s)==1):
+	     return True
+	else:
+	     return False
 
 
 
