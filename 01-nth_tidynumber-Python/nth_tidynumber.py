99a840a781bc5b860ea3b9e3e4a8a1d5161d95ad
Rachana <rachanabalabadra29@msitprogram.net>
2021-08-26 11:47:00 +0530

    nth_tidynumber

--- 01-nth_tidynumber-Python/nth_tidynumber.py
+++ 01-nth_tidynumber-Python/nth_tidynumber.py
@@ -6,5 +6,37 @@
 # fun_nth_tidynumber(15) = 17
 # fun_nth_tidynumber(35) = 46
 
+
+
+def noOfDigits(n):
+	c=0
+	while(n>0):
+		n=n//10
+		c+=1
+	return c
+
+def isTidyNumber(n):
+    r=0
+    s=n%10
+    c=0
+    nDigits=noOfDigits(n)
+    for i in range(nDigits):
+        r=n%10
+        n=n//10
+        if(r<=s):
+            c+=1
+        s=r
+    if(c==nDigits):
+        return True
+    else:
+        return False
+
 def fun_nth_tidynumber(n):
-    return 0
+    i=0
+    c=-1
+    while(c<=n):
+        if(isTidyNumber(i)==True):
+            c+=1
+        i+=1
+    return(i-1)
+
