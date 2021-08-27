5352e102ae890627def60e3e3ed99702665adca2
Rachana <rachanabalabadra29@msitprogram.net>
2021-08-27 16:46:29 +0530

    longestcommonsubstring

--- 05-longestcommonsubstring-Python/longestcommonsubstring.py
+++ 05-longestcommonsubstring-Python/longestcommonsubstring.py
@@ -6,7 +6,54 @@
 # If there are two or more longest common substrings, return the lexicographically smaller one (ie, just use "<" to 
 # compare the strings). So, for example:
 # longestCommonSubstring("abcABC", "zzabZZAB") returns "AB" and not "ab"
+# ab, bc, cA, AB, BC
+# res = "AB"
+
+def getAllSubstrings(s, n):
+    # abcdsdsf = 7
+    # 4
+    # abcd,  bcds,  cdsd,  dsds,  sdsf
+    # [0..3] [1..4] [2..5] [3..6] [4..7]
+    # for i in range(4):
+    #   s[i:i+n]
+    res = []
+    if n>len(s): return res
+    for i in range(len(s)-n+1):
+        res.append(s[i:i+n])
+    return res
 
 def longestcommonsubstring(s1, s2):
     # Yourcode goes here
-    pass
+    n = len(s1)
+    m = len(s2)
+    x = min(n,m)
+    if x==n:
+        l = n
+        res = "z"
+        found = False
+        while(l>0):
+            sub = getAllSubstrings(s1, l) # abc, bcd, cde, def
+            for substr in sub:
+                if substr in s2 and substr < res:
+                    res = substr
+                    found = True
+            if found: return res
+            l -= 1
+        return ""
+    else:
+        l = m
+        res = "z"
+        found = False
+        while(l>0):
+            sub = getAllSubstrings(s2, l)
+            for substr in sub:
+                if substr in s1 and substr < res:
+                    res = substr
+                    found = True
+            if found: return res
+            l -= 1
+        return ""
+
+print(longestcommonsubstring("abcABC", "zzabZZAB"))
+print(longestcommonsubstring("abcdef", "abqrcdest"))
+print("cde"<"abqrcdest")
