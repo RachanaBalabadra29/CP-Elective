e4c0caa565aa4c1a2c54ed62ed41b540363b7143
Rachana <rachanabalabadra29@msitprogram.net>
2021-08-31 21:20:40 +0530

    nthcircularprime

--- 08-nth_circularprime-Python/nthcircularprime.py
+++ 08-nth_circularprime-Python/nthcircularprime.py
@@ -5,6 +5,54 @@
 # 5, 7, 11, 13, 17, 31, 37, 71, 73, 79, 97, 113, 131, 197... To see why 197 is a Circular prime, 
 # note that 197 is prime, as is 971 (rotated left), as is 719 (rotated left again).
 
+
+def isPrime(n):
+	if(n<2):
+		return False
+	for i in range(2,n):
+		if(n%i==0):
+			return False
+	return True
+
+
+def noOfDigits(n):
+	c=0
+	while(n>0):
+		n=n//10
+		c+=1
+	return c
+
+def rotate(n):
+	nDigits=noOfDigits(n)
+	r=n%10
+	n=n//10
+	return r*(10**(nDigits-1)) + n
+
+def isZeroPresent(n):
+	while(n>0):
+		r=n%10
+		if(r==0):
+			return True
+		n=n//10
+	return False		
+
+def isCircularPrime(n):
+	if(isZeroPresent(n)==True):
+		return False
+	nDigits=noOfDigits(n)
+	for i in range(nDigits):
+		n=rotate(n)
+		if(isPrime(n) == False):
+			return False
+	return True	
+
 def nthcircularprime(n):
-	# Your code goes here
-	pass
+	i=0
+	c=0
+	while(c<=n):
+		if(isCircularPrime(i)==True):
+			c+=1
+		i+=1
+	return(i-1)	
+
+
